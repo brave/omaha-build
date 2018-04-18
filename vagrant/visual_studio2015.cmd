59a477b348e6ea83dbff92faa3a500971f417a36
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2018-04-17 19:11:55 -0500

    A few fixes

--- vagrant/visual_studio2015.cmd
+++ vagrant/visual_studio2015.cmd
@@ -3,5 +3,5 @@ cd \tmp
 
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "choco install visualstudio2015community -y -packageParameters '--AdminFile c:\vs_2015.xml'" 
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Enable-WindowsOptionalFeature -Online -FeatureName 'NetFx3'" 
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "choco install visualstudio2015community -y -packageParameters '--AdminFile C:\Windows\Temp\vs_2015.xml'"
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Enable-WindowsOptionalFeature -Online -FeatureName 'NetFx3'"
